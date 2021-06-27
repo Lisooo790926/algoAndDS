fb44f1115ebea10f646cf5d0ae23f61152489408
Evis Chnag <evis.chang@seidor.es>
2021-06-27 18:27:14 +0800

    add the remove method for BST

--- class_js_code/class_124_binary_tree.js
+++ class_js_code/class_124_binary_tree.js
@@ -38,17 +38,20 @@ class BinarySearchTree {
         }
     }
 
-    lookup(value) {
+    lookup(value, showLastNode) {
         let currentNode = this.root;
         let currentValue;
+        let lastNode;
 
         while (currentNode !== null && currentNode !== undefined) {
 
             currentValue = currentNode.value;
 
             if (value === currentValue) {
-                return currentNode;
+                return showLastNode ? { "last": lastNode, "current": currentNode } : currentNode;
             }
+
+            lastNode = currentNode;
 
             if (value > currentValue) {
                 currentNode = currentNode.right;
@@ -56,7 +59,66 @@ class BinarySearchTree {
                 currentNode = currentNode.left;
             }
         }
-        return "not found"
+        return false;
+    }
+
+    /**
+     * search for the most left side one
+     * 
+     * @param {*} node for given node
+     * @param {*} isRemoveLastPointer for is cleaning lastNode pointer
+     * @returns the most left side node otherwise return null 
+     */
+    lookupMostLeft(node, isRemoveLastPointer) {
+        if (!node) {
+            return null;
+        }
+
+        let lastNode;
+        while (node) {
+            if (node.left === null) {
+                isRemoveLastPointer ? lastNode.left = null : null;
+                return node
+            }
+            lastNode = node;
+            node = node.left;
+        }
+    }
+
+    // my trial, search for the closest one and replace it
+    remove(value) {
+        // initial condition
+        let replacedNode = null;
+        const searchNodes = this.lookup(value, true);
+
+        if (searchNodes.current) {
+            const deletedNode = searchNodes.current;
+
+            if (deletedNode.right !== null && deletedNode.left !== null) {
+                // if two children, keep searching rightChild's left
+                replacedNode = this.lookupMostLeft(deletedNode.right, true);
+                replacedNode.right = deletedNode.right;
+
+                // if the replacedNode is not the same left, point to the left
+                if (replacedNode !== deletedNode.left) {
+                    replacedNode.left = deletedNode.left;
+                }
+
+            } else if (deletedNode.right !== null || deletedNode.left !== null) {
+                // if leaf child, then replace it
+                let isRight = deletedNode.right !== null;
+                replacedNode = isRight ? deletedNode.right : deletedNode.left;
+            }
+
+            if (replacedNode && searchNodes.last) {
+                // change last node pointer
+                value > searchNodes.last.value ? searchNodes.last.right = replacedNode : searchNodes.last.left = replacedNode;
+            } else if(replacedNode && searchNodes.last === undefined && deletedNode === this.root) {
+                // it's root node
+                this.root = replacedNode;
+            }
+        }
+        return replacedNode !== null;
     }
 }
 
@@ -78,6 +140,15 @@ console.log(tree.lookup(20))
 //     9
 //  4     20
 // 1 6  15 170 
+//       18 
+//      1619
+tree.insert(18);
+tree.insert(16);
+tree.insert(19);
+tree.insert(14);
+
+console.log(tree.remove(15));
+console.log(JSON.stringify(traverse(tree.root)))
 
 // recursive method to get whole tree for checking
 function traverse(node) {
