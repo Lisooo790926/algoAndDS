956a0c1a736ad2ddd217a57bf4397a23af9e8253
Evis Chang <evis.a.chang@accenture.com>
2021-10-03 10:38:56 +0800

    interview question practice

---
+++ class_js_code/class_185_sorting_interview..js
@@ -0,0 +1,59 @@
+//#1 - Sort 10 schools around your house by distance:
+// use graph to define the node and distance
+// use heap sorting to sort because there is no depth 
+
+// class - solution 
+because of 10 size,  small data
+insertion sort
+
+//#2 - eBay sorts listings by the current Bid amount:
+// because already sorted it 
+// so use insertion sort 
+
+// if no already sort
+// use merge or quick sorting
+
+// if amount is small
+// use radix sort
+
+// class - solution
+radix or counting sort
+
+//#3 - Sport scores on ESPN
+// because already sorted it
+// so use insertion sort 
+
+// class - solution 
+Quick sort, consider that memory 
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+// space saving 
+// use heap sorting
+
+// class - solution
+Merge sort, because worry about performance
+quick sort has the worst case, so use merge sort
+
+// this maybe better to use heap sort??? 
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+// insertion sort
+
+// class - solution
+Insertion sort, because already sorted
+
+//#6 - Temperature Records for the past 50 years in Canada
+// merge sort or quick sort or counting sort
+
+// class - solution 
+if all integer number : radix or counting sort
+if not : quick sort
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+// merge sort if enough memeory 
+
+// class - solution 
+quick sort if want the performance
+
+//#8 - You want to teach sorting for the first time
+// bubble sort or selection sort
