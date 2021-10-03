dea37f174e1682689267d9ec55627b1348e1989b
Evis Chang <evis.a.chang@accenture.com>
2021-10-03 21:41:27 +0800

    understanding BFS and DFS

---
+++ class_js_code/class_197_bfs_dfs.js
@@ -0,0 +1,21 @@
+//If you know a solution is not far from the root of the tree:
+BFS 
+
+//If the tree is very deep and solutions are rare: 
+DFS  
+// DFS will take long, because tree is so deep
+// take so long to finish one branch (we are using recursive)
+-> BFS
+
+//If the tree is very wide:
+DFS 
+memorySaving (BFS will take too much memory)
+
+//If solutions are frequent but located deep in the tree:
+DFS 
+
+//Determining whether a path exists between two nodes:
+DFS
+
+//Finding the shortest path:
+BFS
