687509674fb767a183d194028e1bbc97a9b2c802
Evis Chang <evis.a.chang@accenture.com>
2021-10-28 09:49:09 +0800

    461 hamming distance solution

---
+++ leetcode_or_other/array/461_hammingDistance.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function (x, y) {
+  // get max , because of digital
+  // loop to compare each residue
+
+  let maxValue = Math.max(x, y);
+  let minValue = maxValue == x ? y : x;
+
+  let result = [];
+  let currentIndex = 0;
+  while (maxValue > 0) {
+    if (minValue % 2 != maxValue % 2) {
+      result.push(currentIndex);
+    }
+    currentIndex++
+    maxValue = Math.floor(maxValue / 2);
+    minValue = Math.floor(minValue / 2);
+  }
+
+  return result.length;
+};
+
+console.log(hammingDistance(1, 4));
