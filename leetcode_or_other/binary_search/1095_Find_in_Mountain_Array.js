3fc539190017488d1846f8176f35ee934806f613
Evis Chang <evis.a.chang@accenture.com>
2021-11-18 09:04:03 +0800

    1095 find in mountain array, add solution with 3 BS

---
+++ leetcode_or_other/binary_search/1095_Find_in_Mountain_Array.js
@@ -0,0 +1,77 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * function MountainArray() {
+ *     @param {number} index
+ *     @return {number}
+ *     this.get = function(index) {
+ *         ...
+ *     };
+ *
+ *     @return {number}
+ *     this.length = function() {
+ *         ...
+ *     };
+ * };
+ */
+
+/**
+ * @param {number} target
+ * @param {MountainArray} mountainArr
+ * @return {number}
+ */
+ var findInMountainArray = function(target, mountainArr) {
+    
+    const len = mountainArr.length();
+    
+    let current = mountainArr.get(0);
+    let lo = 0;
+    let hi = len - 1;
+    
+    while (hi > lo) {
+        let mid = Math.floor((hi+lo)/2);
+        let value = mountainArr.get(mid);
+        let next = mountainArr.get(mid + 1);
+        if(value < next) {
+            top = mid + 1;
+            lo = top;
+        } else {
+            hi = mid;
+            top = hi;
+        }
+    }
+    
+    lo = 0;
+    hi = top;
+    
+    while (hi >= lo) {
+        let mid = Math.floor((hi+lo)/2);
+        let value = mountainArr.get(mid);
+        if(value > target) {
+            hi = mid - 1;
+        } else if(value < target) {
+            lo = mid + 1;
+        } else {
+            return mid
+        }
+    }
+    
+    lo = top + 1;
+    hi = len - 1;
+    
+    while(hi >= lo) {
+        let mid = Math.floor((hi+lo)/2);
+        let value = mountainArr.get(mid);
+        
+        if(value > target) {
+            lo = mid + 1;
+        } else if(value < target) {
+            hi = mid - 1;
+        } else {
+            return mid
+        }
+    }
+    
+    return -1
+    
+};
